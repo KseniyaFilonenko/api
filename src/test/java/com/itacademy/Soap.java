09866d6df2b351d3bd7cbf8a3fc98394d77a64f9
KseniyaFilonenko <filonenko_k@tut.by>
2024-05-28 15:29:11 +0300

    API tests

--- src/test/java/com/itacademy/Soap.java
+++ src/test/java/com/itacademy/Soap.java
@@ -1,5 +1,6 @@
 package com.itacademy;
 
+import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -10,5 +11,19 @@ public class Soap {
         given().contentType("application/soap+xml").body("")
                 .when().post("https://www.dataaccess.com/webserviceserver/numberconversion.wso")
                 .then().log().all().statusCode(200).extract().response();
+    }
+    @Test
+    public void soapTest2() {
+        Response response = given().contentType("application/soap+xml").body("<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:web=\"http://www.dataaccess.com/webservicesserver/\">\n" +
+                        "   <soapenv:Header/>\n" +
+                        "   <soapenv:Body>\n" +
+                        "      <web:NumberToDollars>\n" +
+                        "         <web:dNum>10</web:dNum>\n" +
+                        "      </web:NumberToDollars>\n" +
+                        "   </soapenv:Body>\n" +
+                        "</soapenv:Envelope>")
+                .when().post("https://www.dataaccess.com/webservicesserver/numberconversion.wso")
+                .then().log().all().statusCode(200).extract().response();
+        System.out.println(response.xmlPath().getString("Envelope.Body.NumberToDollarsResponse.NumberToDollarsResult"));
     }
 }
